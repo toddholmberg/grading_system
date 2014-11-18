0a194aeeb544fbb1abd958a510ee0383aeeaa436
Todd Holmberg <tholmberg+github@gmail.com>
2014-11-18 15:09:04 -0700

    Update DevAdapter.php

--- library/Cop/Auth/DevAdapter.php
+++ library/Cop/Auth/DevAdapter.php
@@ -11,10 +11,8 @@ class Cop_Auth_DevAdapter implements Zend_Auth_Adapter_Interface
 			 * Add code(unid) of desired role to
 			 * circumvent UofU CAS login durin testing 
 			 */
-			//$code = 'u0615627'; // Gisel (admin)
+			
 			$code = 'u9999999'; // admin
-			//$code = 'u07687676'; // faculty
-			//$code = 'u0011967'; // student
 			//$code = false;
 
 			$result = new Zend_Auth_Result(1, $code);
