57b0d18169cfc4dc64d2ea449b3393cefa9476a0
Todd Holmberg <tholmberg+github@gmail.com>
2014-11-18 15:08:28 -0700

    Update Adapter.php

--- library/Cop/Auth/Adapter.php
+++ library/Cop/Auth/Adapter.php
@@ -6,11 +6,7 @@ class Cop_Auth_Adapter implements Zend_Auth_Adapter_Interface
 	public function authenticate()
 	{
 		try {
-			//$code = 'u9090909'; // admin
-			//$code = 'u07687676'; // faculty
-			//$code = 'u0011967'; // student
-			//$code = false;
-
+			
 			// initialize phpCAS
 			phpCAS::client( CAS_VERSION_2_0, 'ulogin.utah.edu', 443, 'cas', false ) ;
 
